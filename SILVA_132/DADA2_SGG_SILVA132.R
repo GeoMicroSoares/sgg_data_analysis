01ed62066869c9f2df92191882bb4ee81788b4bd
GeoMicroSoares <ans74@aber.ac.uk>
2018-02-17 14:21:45 +0000

    SILVA_132 eats up way too much RAM with addSpecies(), not going for that anymore

--- SILVA_132/DADA2_SGG_SILVA132.R
+++ SILVA_132/DADA2_SGG_SILVA132.R
@@ -21,9 +21,10 @@ library(cowplot)
 SGG_SVt = readRDS("/media/andre/B2F8C9A0F8C962E9/SGG_16S_analysis/SGG_16S_data_trimmed_DADA2_table/seqtab_final.rds")
 SILVA_132_tax <- assignTaxonomy(SGG_SVt, "/media/andre/B2F8C9A0F8C962E9/SGG_16S_analysis/SILVA_132/silva_nr_v132_train_set.fa.gz", 
                       multithread=TRUE)
-SILVA_132_taxa <- addSpecies(SILVA_132_tax, "/media/andre/B2F8C9A0F8C962E9/SGG_16S_analysis/SILVA_132/silva_species_assignment_v132.fa.gz")
-# Write to disk
-saveRDS(SILVA_132_taxa, "/media/andre/B2F8C9A0F8C962E9/SGG_16S_analysis/SGG_16S_data_trimmed_DADA2_table/SILVA_132_taxa_final.rds")
+#THIS ATE UP ~99GB RAM, NOT ADDING SPECIES
+# SILVA_132_taxa <- addSpecies(SILVA_132_tax, "/media/andre/B2F8C9A0F8C962E9/SGG_16S_analysis/SILVA_132/silva_species_assignment_v132.fa.gz")
+# # Write to disk
+saveRDS(SILVA_132_tax, "/media/andre/B2F8C9A0F8C962E9/SGG_16S_analysis/SGG_16S_data_trimmed_DADA2_table/SILVA_132_tax_final.rds")
 
 #Check SV table size with phyloseq
 #taxa_are_rows bc of makeSequenceTable()
@@ -34,7 +35,7 @@ sgg_metadata_ord<-sgg_metadata[match(order, sgg_metadata$Sample_Code),]
 rownames(sgg_metadata_ord) <- sgg_metadata_ord$Sample_Code
 ps_132 <- phyloseq(otu_table(SGG_SVt, taxa_are_rows=FALSE),
                sample_data(sgg_metadata_ord),
-               tax_table(SILVA_132_taxa))
+               tax_table(SILVA_132_tax))
 ps_132
 
 ps_132.a = subset_samples(ps_132, Site.name != "Taffs Well PUMPED")
